ff9f675f6665f039153cc7502f80e904cbb50471
nickhill <nickhill@us.ibm.com>
2021-10-05 09:10:12 -0700

    refac: A few simplifications/optimizations to transcoding logic
    This is just a first pass, the documented TODOs are still to be done, as well as unit tests of course :)

--- proxy/marshaler.go
+++ proxy/marshaler.go
@@ -6,10 +6,26 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"io/ioutil"
+	"reflect"
 
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	gw "github.com/kserve/rest-proxy/gen"
+)
+
+const (
+	BOOL   = "BOOL"
+	UINT8  = "UINT8"
+	UINT16 = "UINT16"
+	UINT32 = "UINT32"
+	UINT64 = "UINT64"
+	INT8   = "INT8"
+	INT16  = "INT16"
+	INT32  = "INT32"
+	INT64  = "INT64"
+	FP16   = "FP16"
+	FP32   = "FP32"
+	FP64   = "FP64"
+	BYTES  = "BYTES"
 )
 
 type Tensor struct {
@@ -39,189 +55,111 @@ type CustomJSONPb struct {
 	runtime.JSONPb
 }
 
+type tensorType struct {
+	size      int
+	sliceType reflect.Type
+}
+
 // Sizes of each type in bytes.
-var typeSizes = map[string]int64{
-	"BOOL":   1,
-	"UINT8":  1,
-	"UINT16": 2,
-	"UINT32": 4,
-	"UINT64": 8,
-	"INT8":   1,
-	"INT16":  2,
-	"INT32":  4,
-	"INT64":  8,
-	"FP16":   2,
-	"FP32":   4,
-	"FP64":   8,
-	"BYTES":  1,
+var tensorTypes = map[string]tensorType{
+	BOOL:   {1, sliceType(true)},
+	UINT8:  {1, sliceType(uint8(0))},
+	UINT16: {2, sliceType(uint16(0))},
+	UINT32: {4, sliceType(uint32(0))},
+	UINT64: {8, sliceType(uint64(0))},
+	INT8:   {1, sliceType(int8(0))},
+	INT16:  {2, sliceType(int16(0))},
+	INT32:  {4, sliceType(int32(0))},
+	INT64:  {8, sliceType(int64(0))},
+	FP16:   {2, nil}, //TODO
+	FP32:   {4, sliceType(float32(0))},
+	FP64:   {8, sliceType(float64(0))},
+	BYTES:  {1, sliceType(byte(0))},
 }
 
 // This function adjusts the gRPC response before marshaling and
 // returning to the user.
 func (c *CustomJSONPb) Marshal(v interface{}) ([]byte, error) {
-	var err error
-	var j []byte
-	switch v := v.(type) {
-	case *gw.ModelInferResponse:
+	if r, ok := v.(*gw.ModelInferResponse); ok {
 		resp := &RESTResponse{}
-		resp.ModelName = v.ModelName
-		resp.ModelVersion = v.ModelVersion
-		resp.Id = v.Id
-		resp.Parameters = v.Parameters
+		resp.ModelName = r.ModelName
+		resp.ModelVersion = r.ModelVersion
+		resp.Id = r.Id
+		resp.Parameters = r.Parameters
+		resp.Outputs = make([]Tensor, len(r.Outputs))
 
-		for index, output := range v.Outputs {
-			tensor := Tensor{}
+		for index, output := range r.Outputs {
+			tensor := &resp.Outputs[index]
 			tensor.Name = output.Name
 			tensor.Datatype = output.Datatype
 			tensor.Shape = output.Shape
 			tensor.Parameters = output.Parameters
 
-			var numElements int64 = 1
-			for j := range tensor.Shape {
-				numElements = tensor.Shape[j] * numElements
+			if r.RawOutputContents != nil {
+				tt, ok := tensorTypes[tensor.Datatype]
+				if !ok {
+					return nil, fmt.Errorf("unsupported datatype in inference response outputs: %s",
+						tensor.Datatype)
+				}
+				switch tensor.Datatype {
+				case BYTES:
+					tensor.Data = r.RawOutputContents[index]
+				case FP16:
+					return nil, fmt.Errorf("FP16 tensors not supported (request tensor %s)", tensor.Name) //TODO
+				default:
+					numElements := int(elementCount(tensor.Shape))
+					var err error
+					if tensor.Data, err = readBytes(r.RawOutputContents[index], tt, 0, numElements); err != nil {
+						return nil, err
+					}
+				}
+			} else {
+				switch tensor.Datatype {
+				case BOOL:
+					tensor.Data = output.Contents.BoolContents
+				case UINT8, UINT16, UINT32:
+					tensor.Data = output.Contents.UintContents
+				case UINT64:
+					tensor.Data = output.Contents.Uint64Contents
+				case INT8, INT16, INT32:
+					tensor.Data = output.Contents.IntContents
+				case INT64:
+					tensor.Data = output.Contents.Int64Contents
+				case FP16:
+					return nil, fmt.Errorf("FP16 tensors not supported (request tensor %s)", tensor.Name) //TODO
+				case FP32:
+					tensor.Data = output.Contents.Fp32Contents
+				case FP64:
+					tensor.Data = output.Contents.Fp64Contents
+				case BYTES:
+					tensor.Data = output.Contents.BytesContents
+				default:
+					return nil, fmt.Errorf("unsupported datatype in inference response outputs: %s",
+						tensor.Datatype)
+				}
 			}
-
-			var data interface{}
-			switch tensor.Datatype {
-			case "BOOL":
-				if v.RawOutputContents == nil {
-					data = output.Contents.BoolContents
-				} else {
-					outputData := make([]bool, numElements)
-					err = readBytes(v.RawOutputContents[index], &outputData, 0, typeSizes[tensor.Datatype], numElements)
-					data = outputData
-				}
-			case "UINT8":
-				if v.RawOutputContents == nil {
-					data = output.Contents.UintContents
-				} else {
-					outputData := make([]uint8, numElements)
-					err = readBytes(v.RawOutputContents[index], &outputData, 0, typeSizes[tensor.Datatype], numElements)
-					data = outputData
-				}
-			case "UINT16":
-				if v.RawOutputContents == nil {
-					data = output.Contents.UintContents
-				} else {
-					outputData := make([]uint16, numElements)
-					err = readBytes(v.RawOutputContents[index], &outputData, 0, typeSizes[tensor.Datatype], numElements)
-					data = outputData
-				}
-			case "UINT32":
-				if v.RawOutputContents == nil {
-					data = output.Contents.UintContents
-				} else {
-					outputData := make([]uint32, numElements)
-					err = readBytes(v.RawOutputContents[index], &outputData, 0, typeSizes[tensor.Datatype], numElements)
-					data = outputData
-				}
-			case "UINT64":
-				if v.RawOutputContents == nil {
-					data = output.Contents.Uint64Contents
-				} else {
-					outputData := make([]uint64, numElements)
-					err = readBytes(v.RawOutputContents[index], &outputData, 0, typeSizes[tensor.Datatype], numElements)
-					data = outputData
-				}
-			case "INT8":
-				if v.RawOutputContents == nil {
-					data = output.Contents.IntContents
-				} else {
-					outputData := make([]int8, numElements)
-					err = readBytes(v.RawOutputContents[index], &outputData, 0, typeSizes[tensor.Datatype], numElements)
-					data = outputData
-				}
-			case "INT16":
-				if v.RawOutputContents == nil {
-					data = output.Contents.IntContents
-				} else {
-					outputData := make([]int16, numElements)
-					err = readBytes(v.RawOutputContents[index], &outputData, 0, typeSizes[tensor.Datatype], numElements)
-					data = outputData
-				}
-			case "INT32":
-				if v.RawOutputContents == nil {
-					data = output.Contents.IntContents
-				} else {
-					outputData := make([]int32, numElements)
-					err = readBytes(v.RawOutputContents[index], &outputData, 0, typeSizes[tensor.Datatype], numElements)
-					data = outputData
-				}
-			case "INT64":
-				if v.RawOutputContents == nil {
-					data = output.Contents.Int64Contents
-				} else {
-					outputData := make([]int64, numElements)
-					err = readBytes(v.RawOutputContents[index], &outputData, 0, typeSizes[tensor.Datatype], numElements)
-					data = outputData
-				}
-			case "FP16":
-				// TODO: Relies on raw_input_contents
-			case "FP32":
-				if v.RawOutputContents == nil {
-					data = output.Contents.Fp32Contents
-				} else {
-					outputData := make([]float32, numElements)
-					err = readBytes(v.RawOutputContents[index], &outputData, 0, typeSizes[tensor.Datatype], numElements)
-					data = outputData
-				}
-			case "FP64":
-				if v.RawOutputContents == nil {
-					data = output.Contents.Fp64Contents
-				} else {
-					outputData := make([]float64, numElements)
-					err = readBytes(v.RawOutputContents[index], &outputData, 0, typeSizes[tensor.Datatype], numElements)
-					data = outputData
-				}
-			case "BYTES":
-				if v.RawOutputContents == nil {
-					data = output.Contents.BytesContents
-				} else {
-					data = v.RawOutputContents[index]
-				}
-			default:
-				return nil, fmt.Errorf("Unsupported Datatype in inference response outputs")
-			}
-
-			if err != nil {
-				return nil, err
-			}
-
-			tensor.Data = data
-			resp.Outputs = append(resp.Outputs, tensor)
-
-			j, err = c.JSONPb.Marshal(resp)
 		}
-	default:
-		j, err = c.JSONPb.Marshal(v)
+		v = resp
 	}
 
-	if err != nil {
-		return nil, err
-	}
-	return j, nil
+	return c.JSONPb.Marshal(v)
 }
 
-// This function adjusts the user input before a gRPC message is sent to the
-// server.
+// This function adjusts the user input before a gRPC message is sent to the server.
 func (c *CustomJSONPb) NewDecoder(r io.Reader) runtime.Decoder {
 	return runtime.DecoderFunc(func(v interface{}) error {
 		req, ok := v.(*gw.ModelInferRequest)
 		if ok {
 			logger.Info("Received REST inference request")
-			raw, err := ioutil.ReadAll(r)
-			if err != nil {
-				return err
-			}
 			restReq := RESTRequest{}
-			err = json.Unmarshal(raw, &restReq)
-			if err != nil {
+			if err := json.NewDecoder(r).Decode(&restReq); err != nil {
 				return err
 			}
 
 			req.Id = restReq.Id
 			req.Parameters = restReq.Parameters
 			req.Outputs = restReq.Outputs
+			req.Inputs = make([]*gw.ModelInferRequest_InferInputTensor, 0, len(restReq.Inputs))
 
 			// TODO: Figure out better/cleaner way to do type coercion?
 			// TODO: Flatten N-Dimensional data arrays.
@@ -235,51 +173,51 @@ func (c *CustomJSONPb) NewDecoder(r io.Reader) runtime.Decoder {
 				}
 				d := input.Data.([]interface{})
 				switch tensor.Datatype {
-				case "BOOL":
+				case BOOL:
 					data := make([]bool, len(d))
 					for i := range d {
 						data[i] = d[i].(bool)
 					}
 					tensor.Contents = &gw.InferTensorContents{BoolContents: data}
-				case "UINT8", "UINT16", "UINT32":
+				case UINT8, UINT16, UINT32:
 					data := make([]uint32, len(d))
 					for i := range d {
 						data[i] = uint32(d[i].(float64))
 					}
 					tensor.Contents = &gw.InferTensorContents{UintContents: data}
-				case "UINT64":
+				case UINT64:
 					data := make([]uint64, len(d))
 					for i := range d {
 						data[i] = uint64(d[i].(float64))
 					}
 					tensor.Contents = &gw.InferTensorContents{Uint64Contents: data}
-				case "INT8", "INT16", "INT32":
+				case INT8, INT16, INT32:
 					data := make([]int32, len(d))
 					for i := range d {
 						data[i] = int32(d[i].(float64))
 					}
 					tensor.Contents = &gw.InferTensorContents{IntContents: data}
-				case "INT64":
+				case INT64:
 					data := make([]int64, len(d))
 					for i := range d {
 						data[i] = int64(d[i].(float64))
 					}
 					tensor.Contents = &gw.InferTensorContents{Int64Contents: data}
-				case "FP16":
-					// TODO: Relies on raw_input_contents
-				case "FP32":
+				case FP16:
+					return fmt.Errorf("FP16 tensors not supported (response tensor %s)", tensor.Name) //TODO
+				case FP32:
 					data := make([]float32, len(d))
 					for i := range d {
 						data[i] = float32(d[i].(float64))
 					}
 					tensor.Contents = &gw.InferTensorContents{Fp32Contents: data}
-				case "FP64":
+				case FP64:
 					data := make([]float64, len(d))
 					for i := range d {
 						data[i] = d[i].(float64)
 					}
 					tensor.Contents = &gw.InferTensorContents{Fp64Contents: data}
-				case "BYTES":
+				case BYTES:
 					// TODO: BytesContents is multi-dimensional. Figure out how to
 					// correctly represent the data from a 2D slice.
 					data := make([][]byte, 1)
@@ -289,7 +227,7 @@ func (c *CustomJSONPb) NewDecoder(r io.Reader) runtime.Decoder {
 					}
 					tensor.Contents = &gw.InferTensorContents{BytesContents: data}
 				default:
-					return fmt.Errorf("Unsupported Datatype")
+					return fmt.Errorf("unsupported datatype: %s", tensor.Datatype)
 				}
 				req.Inputs = append(req.Inputs, tensor)
 			}
@@ -299,9 +237,23 @@ func (c *CustomJSONPb) NewDecoder(r io.Reader) runtime.Decoder {
 	})
 }
 
+func elementCount(shape []int64) int64 {
+	var count int64 = 1
+	for j := range shape {
+		count *= shape[j]
+	}
+	return count
+}
+
 // This function is used for processing RawOutputContents byte array.
-func readBytes(dataBytes []byte, data interface{}, index int, size int64, numElements int64) error {
-	start := int64(index) * numElements * size
-	buf := bytes.NewBuffer(dataBytes[start : start+numElements*size])
-	return binary.Read(buf, binary.LittleEndian, data)
+func readBytes(dataBytes []byte, elementType tensorType, index int, numElements int) (interface{}, error) {
+	tensorSize := numElements * elementType.size
+	start := index * tensorSize
+	buf := bytes.NewBuffer(dataBytes[start : start+tensorSize])
+	data := reflect.MakeSlice(elementType.sliceType, numElements, numElements).Interface()
+	return &data, binary.Read(buf, binary.LittleEndian, &data)
+}
+
+func sliceType(v interface{}) reflect.Type {
+	return reflect.SliceOf(reflect.TypeOf(v))
 }
